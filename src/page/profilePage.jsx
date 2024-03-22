67c7e32753f25a342d68f0a45afe92a5d2d0ed03
Jans <janshenfu.y.t@gmail.com>
2024-03-22 10:25:41 +0700

    commit

--- src/page/profilePage.jsx
+++ src/page/profilePage.jsx
@@ -1,58 +1,15 @@
 import { TextField } from "@mui/material";
 import React from "react";
+import EditProfile from "../component/editProfile";
+import Sidebar from "../component/sidebar";
 
 function ProfilePage() {
     return (
-        <section className="back p-4">
-            <div className="text-left">
-                <h1>Edit Profile</h1>
-            </div>
-            <div className="flex flex-col items-center">
-                <div className="w/12">
-                    <div>
-                        <img src="" alt="" />
-                    </div>
-                    <div>
-                        <h1>Change Profile</h1>
-                    </div>
-                    <form action="">
-                        <div className="mb-4 mt-4">
-                            <TextField
-                                type="text"
-                                label="email"
-                                size="small"
-                                required
-                                fullWidth
-                            />
-                        </div>
-                        <div className="mb-4 mt-4">
-                            <TextField
-                                type="text"
-                                label="Username"
-                                size="small"
-                                required
-                                fullWidth
-                            />
-                        </div>
-                        <div className="mb-4 mt-4">
-                            <TextField
-                                type="text"
-                                label="birthdate"
-                                size="small"
-                                required
-                                fullWidth
-                            />
-                        </div>
-                        <div className="mb-4 mt-4">
-                            <TextField
-                                type="text"
-                                label="PhoneNumber"
-                                size="small"
-                                required
-                                fullWidth
-                            />
-                        </div>
-                    </form>
+        <section>
+            <div className="flex">
+                <Sidebar />
+                <div className="ml-64 w-full">
+                    <EditProfile />
                 </div>
             </div>
         </section>
